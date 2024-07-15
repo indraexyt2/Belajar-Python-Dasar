789e07ee072b44b07247a389aa694297074c072f
Indrawansyah <62201477+indraexyt2@users.noreply.github.com>
2024-07-16 00:09:17 +0700

    Add files via upload

---
+++ 01. Tipe data/main.py
@@ -0,0 +1,19 @@
+# tipe data: Angka satuan (Integer)
+data_integer = 11
+print("data : ", data_integer)
+print("bertipe : ", type(data_integer))
+
+# tipe data: angka desimal (Float)
+data_float = 3.5
+print("data : ", data_float)
+print("bertipe : ", type(data_float))
+
+# tipe data: kumpulan karakter (String)
+data_string = "Harga"
+print("data : ", data_string)
+print("bertipe : ", type(data_string))
+
+# tipe data: biner true/false (Boolean)
+data_bool = False
+print("data : ", data_bool)
+print("bertipe : ", type(data_bool))
