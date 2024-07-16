440257012b2354892e57abdd91d349d62961d426
Indrawansyah <62201477+indraexyt2@users.noreply.github.com>
2024-07-16 14:13:08 +0700

    Update main.py

--- 03. Aritmatika/main.py
+++ 03. Aritmatika/main.py
@@ -30,3 +30,38 @@ print(a,'%',b,'=',hasil)
 # Floor division // Pembagian yang dibulatkan
 hasil = a // b
 print(a,'//',b,'=',hasil)
+
+# Meminta data pada user dan melakukan operasi artimatika
+
+# Penjumlahan 2 angka
+Data1 = int(input("Masukan data: "))
+Data2 = int(input("Masukan data: "))
+hasil = Data1 + Data2
+print("Hasilnya adalah "  + str(hasil))
+
+# Pengurangan 2 angka
+Data1 = int(input("Masukan data: "))
+Data2 = int(input("Masukan data: "))
+hasil = Data1 - Data2
+print("Hasilnya adalah "  + str(hasil))
+
+# Pembagian 2 angka
+Data1 = int(input("Masukan data: "))
+Data2 = int(input("Masukan data: "))
+hasil = Data1 / Data2
+print("Hasilnya adalah "  + str(hasil))
+
+# Eksponen
+Data1 = int(input("Masukan data: "))
+Data2 = int(input("Masukan data: "))
+hasil = Data1 ** Data2
+print("Hasilnya adalah "  + str(hasil))
+
+# ==== Catatan ==== #
+# Tanda kurung akan membuat hasil dihitung duluan
+
+Data1 = int(input("Masukan data: "))
+Data2 = int(input("Masukan data: "))
+Data3 = int(input("Masukan data: "))
+hasil = Data1 * Data2 * (Data3 + Data1)
+print("Hasilnya adalah "  + str(hasil))
