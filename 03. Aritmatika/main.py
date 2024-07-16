4395d66c6bea1e9304cf6958b8dde0ca33c768a8
Indrawansyah <62201477+indraexyt2@users.noreply.github.com>
2024-07-16 13:41:53 +0700

    Add files via upload

---
+++ 03. Aritmatika/main.py
@@ -0,0 +1,32 @@
+# Aritmatika
+
+a = 10
+b = 5
+
+# Pertambahan +
+hasil = a + b
+print(a,'+',b,'=',hasil)
+
+# Pengurangan -
+hasil = a - b
+print(a,'-',b,'=',hasil)
+
+# Perkalian *
+hasil = a * b
+print(a,'*',b,'=',hasil)
+
+# Pembagian /
+hasil = a / b
+print(a,'/',b,'=',hasil)
+
+# Eksponen (pangkat) **
+hasil = a ** b
+print(a,'**',b,'=',hasil)
+
+# Modulus % --- Sisa pembagian, mis. 10 mod 3 = 1
+hasil = a % b
+print(a,'%',b,'=',hasil)
+
+# Floor division // Pembagian yang dibulatkan
+hasil = a // b
+print(a,'//',b,'=',hasil)
