fe47b0d0d525935e4a960dd08977f94b5d729595
Indrawansyah <62201477+indraexyt2@users.noreply.github.com>
2024-07-18 18:32:58 +0700

    Add files via upload

---
+++ 09. If else statment/main.py
@@ -0,0 +1,9 @@
+# === If else statment === #
+
+nama_user = str(input("Masukan nama Anda: "))
+
+if nama_user == "Indra":
+    print("Kamu pasti ganteng")
+else :
+    print("Skipp Skipp")
+print("Done")
