40f80e433ecbe0469a2be0f1f13c1f260e9edfb2
Indrawansyah <62201477+indraexyt2@users.noreply.github.com>
2024-07-19 07:24:41 +0700

    Delete 09. If else statment directory

--- 09. If else statment/main.py
+++
@@ -1,9 +0,0 @@
-# === If else statment === #
-
-nama_user = str(input("Masukan nama Anda: "))
-
-if nama_user == "Indra":
-    print("Kamu pasti ganteng")
-else :
-    print("Skipp Skipp")
-print("Done")
