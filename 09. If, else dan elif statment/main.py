d24e669a4414ffc06f41946fb2018c47453bd917
Indrawansyah <62201477+indraexyt2@users.noreply.github.com>
2024-07-19 07:24:57 +0700

    Add files via upload

---
+++ 09. If, else dan elif statment/main.py
@@ -0,0 +1,11 @@
+# === If else statment === #
+
+nama_user = str(input("Masukan nama Anda: "))
+
+if nama_user == "Indra": # Aksi true 1
+    print("Kamu pasti ganteng")
+elif nama_user == "Wansyah": # Aksi true 2
+    print("Kamu pasti ganteng")
+else : # Aksi false
+    print("Gak tau kamu siapa")
+print("Done")
